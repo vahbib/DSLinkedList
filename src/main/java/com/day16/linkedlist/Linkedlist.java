1138f329646cbfd4c2cccbd75555c2c6a17656be
vahbib <singhbibhav084@gmail.com>
2020-10-18 16:59:08 +0530

    Added - Insert element with junit testing

--- src/main/java/com/day16/linkedlist/Linkedlist.java
+++ src/main/java/com/day16/linkedlist/Linkedlist.java
@@ -2,7 +2,7 @@ package com.day16.linkedlist;
 
 
 public class Linkedlist {
-    class Node {
+    static class Node {
         int data;
         Node next;
 
@@ -13,8 +13,9 @@ public class Linkedlist {
     }
 
     public Node head = null;
-    public Node temp= null;
+    public Node temp = null;
 
+    // UserCase 1
     public void addNode(int data) {
         Node newNode = new Node(data);
         if (head == null)
@@ -26,12 +27,176 @@ public class Linkedlist {
         }
         temp = newNode;
     }
+    // UserCase 2
+    public void addAtStart(int data) {
+
+        Node newNode = new Node(data);
+        if (head == null) {
+
+            head = newNode;
+            temp =newNode;
+        }
+        else
+        {
+            Node temp = head;
+            head = newNode;
+            head.next = temp;
+        }
+    }
+    // UserCase 3
+    public void append(int data) {
+
+        Node newNode = new Node(data);
+        if (head == null) {
+
+            head = newNode;
+            temp =newNode;
+        }
+        else
+        {
+            Node temp = head;
+            head = newNode;
+            head.next = temp;
+        }
+    }
+
+    static Node GetNode(int data) {
+        return new Node(data);
+    }
+    /* UC 5 :
+     *delete first element
+     */
+    static Node deleteFirst(Node head)
+    {
+        if (head == null)
+            return null;
+
+        Node temp = head;
+        head = head.next;
+
+        return head;
+    }
+    /* UC 8 :
+     *Insert element
+     */
+
+    public static int Insert(Node head, int data) {
+        if (head == null)
+            return 0;
+
+        if (head.data == data) {
+            return 1;
+        }
+        int c = 1;
+        Node secLast = head;
+        while (secLast.next != null) {
+            c += 1;
+            if (secLast.next.data == data)
+                return c;
+            secLast = secLast.next;
+        }
+
+        return 0;
+    }
+
+    /* UC 5 :
+     *delete last element
+     */
+
+    static Node deleteLast(Node head)
+    {
+        if (head == null)
+            return null;
+
+        if (head.next == null) {
+            return null;
+        }
+        Node secLast = head;
+        while (secLast.next.next != null)
+            secLast = secLast.next;
+
+        // Change next of second last
+        secLast.next = null;
+
+        return head;
+    }
+
+    /* UC 7 :
+     *search element
+     */
+    public static boolean searchList(Node head, int data)
+    {
+        if (head == null)
+            return false;
+
+        if (head.data==data) {
+            return true;
+        }
+        Node secLast = head;
+        while (secLast.next!= null) {
+            if (secLast.next.data == data)
+                return true;
+            secLast = secLast.next;
+        }
+
+        return false;
+    }
+    /* UC 4 :
+     *Insert at given position
+     */
+
+    public static Node InsertPosition(Node headNode, int position, int data) {
+        Node head = headNode;
+        if (position < 1)
+            System.out.print("Invalid position");
+
+        if (position == 1) {
+            Node newNode = new Node(data);
+            newNode.next = headNode;
+            head = newNode;
+        } else {
+            while (position-- != 0) {
+                if (position == 1) {
+                    Node newNode = GetNode(data);
+                    newNode.next = headNode.next;
+                    headNode.next = newNode;
+                    break;
+                }
+                headNode = headNode.next;
+            }
+            if (position != 1)
+                System.out.print("\nPosition out of range");
+        }
+        return head;
+    }
+
+    public static void display(Node head)
+    {
+        Node n = head;
+        if (head == null) {
+            System.out.println("Oops!! Nothing in list");
+            return;
+        }
+        while (n != null)
+        {
+            System.out.print("["+n.data + "]-");
+            n = n.next;
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
 
-        Linkedlist List = new Linkedlist();
-        List.addNode(70);
-        List.addNode(30);
-        List.addNode(56);
+//        Node head = GetNode(56);
+//        head.next = GetNode(30);
+//        head.next.next = GetNode(70);
+//
+//        System.out.print("Before: ");
+//        display(head);
+//        System.out.print("After: ");
+//        head = deleteLast(head);
+//        display(head);
+
+
     }
 }
--- src/test/java/com/day16/linkedlist/test/LLTest.java
+++ src/test/java/com/day16/linkedlist/test/LLTest.java
@@ -1,14 +1,24 @@
-//package com.day16.linkedlist.test;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class LLTest {
-//
-//    @Test
-//    public void testLinkedList() {
-//        // Code Here
-//
-//    }
-//
-//}
+package com.day16.linkedlist.test;
+
+import com.day16.linkedlist.Linkedlist;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LLTest {
+
+    @Test
+    public void testLinkedList() {
+        // Code Here
+        Linkedlist list=new Linkedlist();
+        list.addNode(30);
+        list.addNode(56);
+        list.addNode(70);
+        list.display(list.head);
+        int pos=list.Insert(list.head,56);
+        System.out.println("Pos is: "+pos);
+        list.head=list.InsertPosition(list.head,pos,40);
+        list.display(list.head);
+
+    }
+
+}
