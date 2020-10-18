837b0b407440fb48ca4445d4e8b70abd5c85052a
vahbib <singhbibhav084@gmail.com>
2020-10-18 14:51:49 +0530

    Initialization

---
+++ src/main/java/com/day16/linkedlist/Linkedlist.java
@@ -0,0 +1,37 @@
+package com.day16.linkedlist;
+
+
+public class Linkedlist {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node temp= null;
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else {
+            temp.next = newNode;
+        }
+        temp = newNode;
+    }
+
+    public static void main(String[] args) {
+
+        Linkedlist List = new Linkedlist();
+        List.addNode(70);
+        List.addNode(30);
+        List.addNode(56);
+    }
+}
---
+++ src/test/java/com/day16/linkedlist/test/LLTest.java
@@ -0,0 +1,14 @@
+//package com.day16.linkedlist.test;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//public class LLTest {
+//
+//    @Test
+//    public void testLinkedList() {
+//        // Code Here
+//
+//    }
+//
+//}
