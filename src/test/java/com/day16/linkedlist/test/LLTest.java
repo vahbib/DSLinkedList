a665f2db9416eaebfedfb9d7969d587ba56cc7b6
vahbib <singhbibhav084@gmail.com>
2020-10-18 16:32:46 +0530

    Added - search element with junit testing

--- src/test/java/com/day16/linkedlist/test/LLTest.java
+++ src/test/java/com/day16/linkedlist/test/LLTest.java
@@ -1,14 +1,23 @@
-//package com.day16.linkedlist.test;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class LLTest {
-//
-//    @Test
-//    public void testLinkedList() {
-//        // Code Here
-//
-//    }
-//
-//}
+package com.day16.linkedlist.test;
+
+import com.day16.linkedlist.Linkedlist;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LLTest {
+    /* UC 7 :
+     *search element Test JUnit
+     */
+    @Test
+    public void searchListTest() {
+        // Code Here
+        Linkedlist list=new Linkedlist();
+        list.addNode(30);
+        list.addNode(56);
+        list.addNode(70);
+//        list.display(list.head);
+        Assert.assertTrue(list.searchList(list.head,30));
+
+    }
+
+}
