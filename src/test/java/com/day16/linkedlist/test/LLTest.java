a1eb72abb3ea7e81d1810321fd0bf515bd1538c4
vahbib <singhbibhav084@gmail.com>
2020-10-18 17:23:48 +0530

    Added - Remove element & displaying length with junit testing

--- src/test/java/com/day16/linkedlist/test/LLTest.java
+++ src/test/java/com/day16/linkedlist/test/LLTest.java
@@ -1,14 +1,28 @@
-//package com.day16.linkedlist.test;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class LLTest {
-//
-//    @Test
-//    public void testLinkedList() {
-//        // Code Here
-//
-//    }
-//
-//}
+package com.day16.linkedlist.test;
+
+import com.day16.linkedlist.Linkedlist;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LLTest {
+
+    @Test
+    public void testLinkedList() {
+        // Code Here
+        Linkedlist list=new Linkedlist();
+        list.addNode(30);
+        list.addNode(40);
+        list.addNode(56);
+        list.addNode(70);
+        list.display(list.head);
+        int total1=list.length();
+        System.out.println("List Length: "+total1);
+
+        list.head=list.removeValue(list.head,40);
+        list.display(list.head);
+        int total2=list.length();
+        System.out.println("List Length: "+total2);
+        Assert.assertEquals(3,total2);
+    }
+
+}
